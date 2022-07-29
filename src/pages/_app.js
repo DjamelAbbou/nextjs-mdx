51a541dd1d005225f980553dc1812466a7b646cc
djamelabbou <djamelabbou@outlook.com>
2022-07-29 15:35:34 +0100

    added posts folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '../../styles/globals.css'
+import Navbar from '../components/navbar'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/bio.js
+++ src/pages/bio.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/navbar'
 
 export default function bio() {
   return (
-    <div>
+    <div className='bio-container'>
+        <Navbar />
         <p>Djamel's bio</p>
         <p>Hi i am djamel</p>
     </div>

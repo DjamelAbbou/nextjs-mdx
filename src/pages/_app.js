a7209a3873a6a3ea482dedcc815718f12863aac7
djamelabbou <djamelabbou@outlook.com>
2022-07-29 19:00:40 +0100

    added bootstrap and set up index page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,23 @@
-import '../../styles/globals.css'
-import Navbar from '../components/navbar'
+import "../../styles/globals.css";
+import Navbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next and MDX Blog</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
